ae166ad7248820e190d4eafb2002b381803fecf2
TeleAgentRobot <kingagent47.ar@gmail.com>
2016-03-10 18:05:24 +0330

    Added files via upload

---
+++ chats.lua
@@ -0,0 +1,150 @@
+local function run(msg)
+if msg.text == "hi" then
+	return "Hello bb"
+end
+if msg.text == "Hi" then
+	return "Hello honey"
+end
+if msg.text == "Hello" then
+	return "Hi bb"
+end
+if msg.text == "hello" then
+	return "Hi honey"
+end
+if msg.text == "سلام" then
+	return "سلام عزیزم"
+end
+if msg.text == "خوبی" then
+	return "ممنون شما چطورید؟"
+end
+if msg.text == "خوبی؟" then
+	return "ممنون شما چطورید؟"
+end
+if msg.text == "بای" then
+	return "بای بای"
+end
+if msg.text == "قیمت" then
+	return [[قیمت گروه: 📢
+
+یک ماهه: 1000 تومان 1⃣
+دو ماهه: 2000 تومان 2⃣
+سه ماهه: 3000 تومان 3⃣
+
+مادام العمر: 5000 هزار تومان⭕️
+
+رایگان: از طریق ارسال تبلیغات کانال در یک کانال یا ربات 1K نفر به بالا📣
+
+خرید از دو راه شارژ و واریز به کارت ممکن است✔️
+
+شماره حساب:🏧 6273531307197961
+( بانک تجارت - علی رییسی )
+
+کارت شارژ: ايرانسل 📶
+
+ادمین: @XHACKERX 👤]]
+end
+if msg.text == "ربات" then
+	return "بله؟"
+end
+if msg.text == "مونا" then
+	return "❤️MONA JOON❤️"
+end
+if msg.text == "علی محمد" then
+	return "❤️Ali JOON❤️"
+end
+if msg.text == "مهدی" then
+	return "❤️Mahdi JOON❤️"
+end
+if msg.text == "علیرضا" then
+	return "❤️AliReza JOON❤"
+end
+if msg.text == "سحر" then
+	return "❤️Sahar JOON❤"
+end
+if msg.text == "محدثه" then
+	return "❤Mohadese JOON❤"
+end
+if msg.text == "عماد" then
+	return "❤Emad JOON❤"
+end
+if msg.text == "زهرا" then
+	return "❤Zhahra JOON❤"
+end
+if msg.text == "علی" then
+	return "❤Ali JOON❤"
+end
+if msg.text == "کسری" then
+	return "❤Kasra JOON❤"
+end
+if msg.text == "Salam" then
+	return "Salam aleykom"
+end
+if msg.text == "salam" then
+	return "و علیکم السلام"
+end
+if msg.text == "TeleAgent" then    
+	return "BEst Bot In The World!"
+end
+if msg.text == "teleagent" then
+	return "Best Bot In The World!"
+end
+if msg.text == "Teleagent" then
+	return "Best Bot In The World"
+end
+if msg.text == "TeleAgentbot" then
+	return "Yes?"
+end
+if msg.text == "Agent" then
+	return "What?"
+end
+if msg.text == "bot" then
+	return "hum?"
+end
+if msg.text == "Bot" then
+	return "Huuuum?"
+end
+if msg.text == "?" then
+	return "Hum??"
+end
+if msg.text == "Bye" then
+	return "Babay"
+end
+if msg.text == "bye" then
+	return "Bye Bye"
+end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^[Hh]i$",
+		"^[Hh]ello$",
+		"^[Zz]ac$",
+		"^TeleAgent$",
+		"^سلام$",
+		"^خوبی$",
+		"^بای$",
+		"^ربات$",
+		"^خوبی؟$",
+		"^مونا$",
+		"^علی محمد$",
+		"^مهدی$",
+		"^علیرضا$",
+		"^کسری$",
+		"^سحر$",
+		"^زهرا$",
+		"^محدثه$",
+		"^علی$",
+		"^قیمت$",
+		"^عماد$",
+		"^[Bb]ot$",
+		"^[Tt]eleAgent$",
+		"^[Bb]ye$",
+		"^?$",
+		"^[Ss]alam$",
+		}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
---
+++ ver.lua
@@ -0,0 +1,32 @@
+do
+
+function run(msg, matches)
+  return '⭕️TeleAgent Telegram Bot v1.5⭕'..[[
+  http://uupload.ir/files/4lt0_photo_2016-03-02_15-43-43.jpg
+
+⭕️TeleAgent Bot⭕️
+🛡Anti spam bot🛡
+
+with lots of COOL stuffs!  ⚙
+from now on you can have your own ANTI SPAM Group! just contact to our SUDO for buying GP!🎁
+dont forget to visit our channel : @TeleAgentTeam
+
+Bot SUDO👤
+@XHACKERX
+
+< TeleAgent,Group Manager > ]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/][Te][Ee][Ll][Ee][Aa][Gg][Ee][Nn][Tt]$",
+    "^[Te][Ee][Ll][Ee][Aa][Gg][Ee][Nn][Tt]$",
+    "^[!/]teleagent$",
+    "^teleagent$"
+  }, 
+  run = run 
+}
+
+end
